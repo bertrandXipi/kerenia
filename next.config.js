112d0cb79a71d18f9b2530aed2da5f6a85e58fff
Votre Nom <votre@email.com>
2026-03-06 12:43:33 +0100

    fix: enable standalone output for Cloud Run deployment

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'standalone',
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
